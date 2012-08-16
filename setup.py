0d39d2e50c2162b550824343780dd46ba065862c
Timothy Edmund Crosley <timothy.crosley@gmail.com>
2012-08-15 22:36:57 -0400

    Package improvment

--- setup.py
+++ setup.py
@@ -10,4 +10,5 @@ setup(name='RedisStore',
       url='http://www.simpleinnovation.org/',
       download_url='https://github.com/timothycrosley/RedisStore/blob/master/dist/RedisStore-0.1.tar.gz?raw=true',
       license = "GNU GPLv2",
+      py_modules = ['RedisStore',],
       requires = ['redis',],)
